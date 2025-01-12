3f39b00809882cdaac7f7ef0fcfc110124107fe4
Lukas <77896790+lukasmwerk@users.noreply.github.com>
2025-01-12 00:16:36 -0500

    working prototype

--- data/test_script.py
+++ data/test_script.py
@@ -8,11 +8,10 @@
 
 import pymupdf # imports the pymupdf library
 from collections import Counter
-doc = pymupdf.open("alejo-carpentier-los-pasos-perdidos.pdf") # open a document
-outline = doc.outline
-
 import pathlib
 import re
+from googletrans import Translator
+import asyncio
 
 class Chapter:
     def __init__(self, title, start, stop):
@@ -20,51 +19,67 @@ class Chapter:
         self.page_start = start
         self.page_stop = stop
         self.dictionary = Counter()
-    def process(self, pymudoc: pymupdf.Document, output_doc: str):
+
+    async def process(self, pymudoc: pymupdf.Document, translator, chapter_count:int):
         for page in pymudoc.pages(self.page_start, self.page_stop, 1):
             for word in re.split(r"\s*[,!? ]\s*", page.get_text("text")):
                 # print(word)
-                self.dictionary.update([word])   
-        print(self.dictionary.most_common()[:-50:-1])
+                self.dictionary.update([word])
+                 
+        first_elements = map(lambda x: x[0], self.dictionary.most_common())  
+        translations = await translator.translate(list(first_elements)[::-1], dest='en')
+
+        with open("out"+str(chapter_count)+".txt", 'w') as f:
+            for translation in translations:
+                if translation.origin != translation.text:
+                    print(translation.origin + ":\t"+ translation.text, file=f)
 
 class Document:
     def __init__(self, pymudoc: pymupdf.Document):
         self.pymudoc = pymudoc
         self.chapters = []
+        self.chapter_count = 0
 
     def add_chapter(self, title: str, start_page: int, end_page: int):
         self.chapters.append(Chapter(title, start_page, end_page))
     
-    def process_chapter(self, title, start, stop):
+    async def process_chapter(self, title, start, stop):
         chapter = Chapter(title, start, stop)
-        chapter.process(self.pymudoc, output_doc)
+        await chapter.process(self.pymudoc, self.translator, self.chapter_count)
+        self.chapter_count += 1
     
-    def process_pdf(self, output_doc):
+    async def process_pdf(self, translator, output_doc):
+        self.translator = translator
         self.output_doc = output_doc
-        self.walk_outline(outline)
+        await self.walk_outline(self.pymudoc.outline)
 
-    def walk_outline(self, outline: pymupdf.Outline):
+    async def walk_outline(self, outline: pymupdf.Outline):
         if outline is None:
             return
     
-        end_page = doc.page_count
+        end_page = self.pymudoc.page_count
         if outline.down is not None:
             end_page = outline.down.page
         elif outline.next is not None:
             end_page = outline.next.page
         
-        self.process_chapter(outline.title, outline.page, end_page)
+        await self.process_chapter(outline.title, outline.page, end_page)
 
-        self.walk_outline(outline.down)
-        self.walk_outline(outline.next)
-
-
-document = Document(doc)
-output_doc = 'output.txt'
-open(output_doc, 'w').close()
-document.process_pdf(output_doc)
+        await self.walk_outline(outline.down)
+        await self.walk_outline(outline.next)
 
 
+async def main():
+    translator = Translator()
+    doc = pymupdf.open("alejo-carpentier-los-pasos-perdidos.pdf") # open a document
+    outline = doc.outline
+    document = Document(doc)
+    output_doc = 'output.txt'
+    open(output_doc, 'w').close()
+    await document.process_pdf(translator, output_doc)
+
+if __name__ ==  '__main__':
+    loop = asyncio.run(main())
     
 
 
